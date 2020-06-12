e2927ab3018215effd440273c7c7eee4ddfef842
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-06-12 12:53:20 -0500

    working on ring_buffer, finished reverse.py, only have the names left todo

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.data = []
+        self.cur = 0
     def append(self, item):
-        pass
+        self.data.append(item)
+        if len(self.data) == self.capacity:
+		    
+			self.__class__ = RingBufferFull
+        # if len(self.data) == self.capacity:
+        #     self.data.pop(0)
+        #     self.data.append(item)
+        # else:
+        #     self.data.append(item)
 
     def get(self):
-        pass
+        return self.data
